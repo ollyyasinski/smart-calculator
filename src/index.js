09f1c721b78442ce8b115e6cc1f4e8019f70af27
Volha Yasinskaya <lelay_@mail.ru>
2018-03-08 18:50:10 +0300

    Feature/smart-calculator: solution

--- src/index.js
+++ src/index.js
@@ -1,27 +1,64 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.initialValue = initialValue;
+    this.storage = String(initialValue);
+    this.powStorage = [];
   }
 
   add(number) {
-    // your implementation
+	  this.initialValue += number;
+    this.storage += " + " + String(number);
+    this.powStorage = [];
+	  return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.initialValue -= number;
+    this.powStorage = [];
+    this.storage += " - " + String(number);
+	  return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.storage += " * " + String(number);
+    this.powStorage = [];
+    this.initialValue = eval(this.storage);
+    return this;
   }
 
+
   devide(number) {
-    // your implementation
+    this.storage += " / " + String(number);
+    this.powStorage = [];
+    this.initialValue = eval(this.storage);
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    var previousBase = parseInt(this.storage.match(/[\d-]+$/));
+    if (this.powStorage.length === 0){
+      this.powStorage.push(previousBase);
+      this.powStorage.push(number);
+      var poweredValue = Math.pow(previousBase,number);
+      this.storage = this.storage.replace(/[\d-]+$/, String(poweredValue));
+    }
+    else{
+      this.powStorage.push(number);
+      var newPoweredValue = this.powStorage[this.powStorage.length - 1];
+      for (var i = this.powStorage.length - 2; i > 0; i--){
+        newPoweredValue = Math.pow(this.powStorage[i], newPoweredValue);
+      }
+      var poweredValue = Math.pow(this.powStorage[0],newPoweredValue);
+      this.storage = this.storage.replace(/[\d-]+$/, String(poweredValue));
+    }   
+    this.initialValue = eval(this.storage);
+    return this;
   }
+
+  valueOf() { 
+    return this.initialValue; 
+    }
 }
+
 
 module.exports = SmartCalculator;
